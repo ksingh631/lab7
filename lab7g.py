43eb6cfdfcf6348b9c59743deddd0a50af5fa569
ksingh631 <singhkushagra106@gmail.com>
2024-11-21 11:30:47 -0500

    lab7 initial commit

---
+++ lab7g.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# Student ID: ksingh631
+def function1():
+    a = 'object_function1'
+    print('print() call in function1 on a:',a)
+
+def function2():
+    a = 'function2_object'
+    print('print() call in function2 on a:',a)
